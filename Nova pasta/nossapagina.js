5eb4184cba9924239da29cae40bd47eb45da0a76
HenriqueAMartins <henriqueam75@gmail.com>
2025-04-22 21:55:15 -0300

    Adicionei site sobre mim

---
+++ Nova pasta/nossapagina.js
@@ -0,0 +1,29 @@
+function naoclica(){
+    alert("Eu disse que não era pra clicar, agora manda 10 pila no pix!")
+}
+
+function exercicio1(){
+    let aviso = prompt("Vamos somar os números! (Digite 'ok' para continuar)")
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite mais um número: "))
+    let soma = num1 + num2
+    alert(`A soma dos dois números é igual a: ${soma}`)
+}
+
+function exercicio2(){
+    let aviso = prompt("Vamos dividir os números! (Digite 'ok' para continuar)")
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite mais um número: "))
+    let divisao = num1 / num2
+    alert(`A divisão dos dois números digitados é igual a: ${divisao}`)
+}
+
+function exercicio3(){
+    let aviso = prompt("Vamos dividir os números! (Digite 'ok' para continuar)")
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite mais um número: "))
+    let divisao = num1 / num2
+    alert(`A divisão dos dois números digitados é igual a: ${divisao}`)
+}
+
+
