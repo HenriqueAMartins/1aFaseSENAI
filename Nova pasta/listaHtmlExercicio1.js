0cdda9dccd56adde112db641ee709f9d0b722721
HenriqueAMartins <henriqueam75@gmail.com>
2025-04-23 22:08:14 -0300

    Adicionei lista de exercícios HTML JS

---
+++ Nova pasta/listaHtmlExercicio1.js
@@ -0,0 +1,8 @@
+// 1. Exercício 1 - Saudação Personalizada
+// Solicite ao usuário que digite seu nome usando prompt .
+// Exiba uma saudação personalizada no formato: "Olá, [nome]! Seja bemvindo!" usando alert .
+
+function ola(){
+    let nome = prompt("Digite qual é o seu nome: ")
+    alert(`Olá, ${nome}! Seja bem-vindo!`)
+}
---
+++ Nova pasta/listaHtmlExercicio2.js
@@ -0,0 +1,6 @@
+// 2. Exercício 2 - Calculadora de Soma
+// Peça ao usuário para inserir dois números (você pode usar prompt para
+// pegar os valores como strings e depois convertê-los para números com
+// Number ).
+// Mostre o resultado da soma desses dois números usando alert .
+
